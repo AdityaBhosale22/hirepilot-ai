dd01cf9cd5699d0b75f1e52a6e1d74706f04e3d5
Aditya Bhosale <adityabhosale4388@gmail.com>
2026-07-29 16:32:18 +0530

    schema added

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,6 +5,9 @@ import companyRoutes from "../modules/company/company.routes.js";
 import jobRoutes from "../modules/job/job.routes.js";
 import applicationRoutes from "../modules/application/application.routes.js";
 import resumeRoutes from "../modules/resume/resume.routes.js";
+import interviewRoutes from "../modules/interview/interview.routes.js";
+import dashboardRoutes from "../modules/dashboard/dashboard.routes.js";
+import notificationRoutes from "../modules/notification/notification.routes.js";
 
 const router = Router();
 
@@ -13,5 +16,8 @@ router.use("/companies", companyRoutes);
 router.use("/jobs", jobRoutes);
 router.use("/applications", applicationRoutes);
 router.use("/resumes", resumeRoutes);
+router.use("/interviews", interviewRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/notifications", notificationRoutes);
 
 export default router;
